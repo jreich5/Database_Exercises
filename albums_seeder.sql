398bda4d024fc4bdf6877284ad58e1acff0ea1c3
Justin Reich <justinreich.dev@gmail.com>
2016-09-16 16:32:35 -0500

    New file for adding MySQL entries

---
+++ albums_seeder.sql
@@ -0,0 +1,7 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 65000000, 'Pop, rock, R&B'),
+    ('AC/DC', 'Back in Black', 1980, 50000000, 'Hard Rock'),
+    ('Pink Floyd', 'The Dark Side of the Moon', 1973, 45000000, 'Progressive rock'),
+    ('Whitney Houston', 'The Bodyguard', 1992, 44000000, 'Soundtrack/R&B, soul, pop'),
+    ('Meat Loaf', 'Bat Out of Hell', 1977, 43000000, 'Hard rock, progressive rock');
