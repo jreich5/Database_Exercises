21ac14611cdbf1290e7070f458bda555cbca06f0
Justin Reich <justinreich.dev@gmail.com>
2016-09-16 15:19:06 -0500

    File for first sql file exercise

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(150) NOT NULL DEFAULT 'NONE',
+    name VARCHAR(100) NOT NULL DEFAULT 'NONE',
+    release_date YEAR(4),
+    sales DECIMAL(12,2) NOT NULL DEFAULT 0, 
+    genre CHAR(255) NOT NULL DEFAULT 'UNKNOWN',
+    PRIMARY KEY (id)
+);
+
