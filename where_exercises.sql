b18432ac5bb27ba31436dc6e561631e9abb98c98
Justin Reich <justinreich.dev@gmail.com>
2016-09-19 17:00:37 -0500

    Completed SQL query exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,3 +2,8 @@ SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya','Maya');
 
 SELECT * FROM employees WHERE last_name LIKE 'E%'; 
 
+SELECT * FROM employees WHERE hire_date > '199%';
+
+SELECT * FROM employees WHERE hire_date LIKE '%12-25';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
