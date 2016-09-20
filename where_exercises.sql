02a89306a055e40409c7b2790e7ecd976f4adc84
Justin Reich <justinreich.dev@gmail.com>
2016-09-20 09:24:22 -0500

    Expanded second query to search for names that start or end in 'e'

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,7 +5,11 @@ OR first_name IN ('Maya'))
 AND gender = 'M';
  
 
-SELECT * FROM employees WHERE last_name LIKE 'E%'; 
+SELECT * FROM employees WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E'; 
+
+/* SELECT * FROM employees WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';  */
 
 SELECT * FROM employees WHERE hire_date > '199%';
 
